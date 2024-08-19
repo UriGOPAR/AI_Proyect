d63c518e982bed08c1777484bc71a3595c220f1c
unknown <urigopar889@gmail.com>
2024-08-18 20:15:01 -0600

    fix:Linear regression

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy
 import numpy as np
 
 #columns = ["DateTime","Temperature","Humidity","Wind Speed","general diffuse flows","diffuse flows","Zone 1 Power Consumption","Zone 2  Power Consumption","Zone 3  Power Consumption"]
@@ -82,88 +83,99 @@ df.to_csv('Normalización de datos.csv', index=False)
 '''
 Implementación de la regresión lineal, debido a que mis datos no son clasificados
 '''
+
+samples = df[['Temperature normalized', 'Humidity normalized', 'Wind Speed', 'general diffuse flows', 'diffuse flows']].values.tolist()
+y = df['Zone 1 normalized'].values.tolist()
+
 __erros__ = []
 
 def h(params, sample):
-    #Calcular la hipotesis
+    # Calcular la hipótesis (producto punto de los parámetros y las muestras)
     acum = 0
     for i in range(len(params)):
-        acum = acum + params[i]*sample[i]
+        acum += params[i] * sample[i]
     return acum
 
 def show_error(params, samples, y):
-    #Calcular el error
+    # Calcular el error cuadrático medio
     global __erros__
     error_acum = 0
     for i in range(len(samples)):
         hyp = h(params, samples[i])
         error = hyp - y[i]
-        error_acum = error_acum + error**2
-        #Mean error  es el promedio del error para sacar la desviación estandar
-        mean_error_param = error_acum / len(samples)
-        __erros__.append(mean_error_param)
+        error_acum += error ** 2
+    mean_error_param = error_acum / (2 * len(samples))
+    __erros__.append(mean_error_param)
+    return mean_error_param
+
 def GD(params, samples, y, alfa):
-    #Gradiente descendiente
-    temp = list(params)
-    general_error = 0
+    # Gradiente descendiente
+    m = len(samples)
+    temp = [0] * len(params)
     for j in range(len(params)):
         acum = 0
-        error_acum = 0
-        for i in range(len(samples)):
+        for i in range(m):
             error = h(params, samples[i]) - y[i]
-            acum = acum + error * samples[i][j]
-        temp[j] = params[j] - alfa * (1/len(samples)) * acum
+            acum += error * samples[i][j]
+        temp[j] = params[j] - (alfa / m) * acum
     return temp
+
 def scaling(samples):
-	acum =0
-	samples = numpy.asarray(samples).T.tolist() 
-	for i in range(1,len(samples)):	
-		for j in range(len(samples[i])):
-			acum=+ samples[i][j]
-		avg = acum/(len(samples[i]))
-		max_val = max(samples[i])
-		for j in range(len(samples[i])):
-			samples[i][j] = (samples[i][j] - avg)/max_val  #Mean scaling
-	return numpy.asarray(samples).T.tolist()
+    # Escalado de las características
+    samples_transposed = list(zip(*samples))
+    scaled_samples = []
+    for i in range(len(samples_transposed)):  # Ignorar la primera columna si está presente
+        feature = list(samples_transposed[i])
+        avg = sum(feature) / len(feature)
+        max_val = max(feature)
+        scaled_feature = [(x - avg) / max_val for x in feature]
+        scaled_samples.append(scaled_feature)
+    scaled_samples = list(zip(*scaled_samples))
+    # Convertir las tuplas de nuevo a listas
+    scaled_samples = [list(sample) for sample in scaled_samples]
+    return scaled_samples
 
-samples = df[['Temperature normalized', 'Humidity normalized', 'Wind Speed', 'general diffuse flows', 'diffuse flows']].values.tolist()
-y = df['Zone 1 normalized'].values.tolist()
-
-params = [0, 0, 0, 0, 0, 0]
-
-#tasas de aprendizaje
-
-alfa = 0.0001
-for i in range(len(samples)):
-	if isinstance(samples[i], list):
-		samples[i]=  [1]+samples[i]
-	else:
-		samples[i]=  [1,samples[i]]
-print ("original samples:")
-print (samples)
-print (samples)
-
+# Inicializar los parámetros
+params = [0] * (len(samples[0]) + 1)  # n+1 porque añadimos la columna de unos
+alfa = 0.01
 epochs = 0
 
+# Agregar una columna de unos (para el término independiente)
+samples = [[1] + sample for sample in samples]
+
+samples = scaling(samples)
+
+errors = []
+
+for i in range(len(samples)):
+    if isinstance(samples[i], list):
+        samples[i] = [1] + samples[i]
+    else:
+        samples[i] = [1, samples[i]]
+
+print("Original samples:")
+print(samples)
+
+print("Original samples:")
+print(samples)
 while True:
     oldparams = list(params)
     print("params")
     params = GD(params, samples, y, alfa)
     show_error(params, samples, y)
     print("params")
-    epochs = epochs + 1
-    if(oldparams == params or epochs == 2):   #  local minima is found when there is no further improvement
-        print ("samples:")
+    epochs += 1
+    if oldparams == params or epochs == 2:  # Limitar a 1000 epochs para evitar bucle infinito
+        print("samples:")
         print(samples)
-        print ("final params:")
-        print (params)
+        print("final params:")
+        print(params)
         break
 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 # Visualizar la gráfica de errores
 plt.plot(__erros__)
-plt.xlabel('Epochs')
+plt.xlabel('Iterations')
 plt.ylabel('Mean Squared Error')
 plt.title('Error during Gradient Descent')
 plt.show()
-
